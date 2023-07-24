a9c9478497839a7fcd75ea84fc80efe6cf1e3db7
akachy <112860366+akachy@users.noreply.github.com>
2023-07-24 08:39:21 +0100

    Update views.py

--- katzegram/views.py
+++ katzegram/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render, redirect
-from . forms import SignUpForm, PostForm
+from . forms import SignUpForm, PostForm, CommentForm, ProfilePicForm
 from . models import Post, Profile, Comment
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib import messages
-import random
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
 # Create your views here.
 
 def home(request):
     if request.user.is_authenticated:
-        
         profile = Profile.objects.exclude(user=request.user)
-
         comments = Comment.objects.all().order_by('-created_at')
         posts = Post.objects.all().order_by('-created_at') 
-        return render(request, 'home.html',{"posts":posts,'profile':profile,'comments':comments})
+        comment_form = CommentForm(request.POST or None)
+        if request.method == 'POST':
+            if comment_form.is_valid():
+                form = comment_form.save(commit=False)
+                form.user = request.user
+                
+                form.save()
+        return render(request, 'home.html',{"posts":posts,'profile':profile,'comments':comments,'comment_form':comment_form})
     else:
         return render(request, 'login.html')
 
@@ -59,7 +65,20 @@ def profile(request, pk):
 
     posts = Post.objects.filter(user_id=pk)
     profile = Profile.objects.get(user_id=pk)
-    # pro = Profile.objects.get(user_id=pk).order_by(?)
+    if request.method == 'POST':
+        current_user_profile = request.user.profile
+        action = request.POST['follow']
+        # decide to follow
+        if action == 'unfollow':
+            current_user_profile.follows.remove(profile)
+        elif action == 'follow':
+            current_user_profile.follows.add(profile)
+        
+        # save profile
+
+        current_user_profile.save()
+
+    
     return render(request, 'profile.html',{'profile':profile, 'posts':posts})
 
 def create(request):
@@ -75,4 +94,42 @@ def create(request):
 
     else:
         return render(request,'login.html')
+    
+def post_detail(request, pk):
+    posts = get_object_or_404(Post, id=pk)
+    comments = Comment.objects.all().order_by('-created_at')
+    comment_form = CommentForm(request.POST or None)
+    if request.method == 'POST':
+        if comment_form.is_valid():
+            form = comment_form.save(commit=False)
+            form.user = request.user
+            form.post = posts
+            form.save()
+            return JsonResponse({'message':'success'})
+        else:
+            comment_form = CommentForm()
+    return render(request, 'post_detail.html',{'posts':posts,'comment_form':comment_form,'comments':comments})
+
+def settings(request):
+    if request.user.is_authenticated:
+        profile_user = Profile.objects.get(user__id=request.user.id)
+
+        # get forms
+        profile_form = ProfilePicForm(request.POST or None, request.FILES or None, instance=profile_user)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request,('information updated'))
+            return redirect('profile',pk = request.user.id)
+
+
+
+        return render(request, 'settings.html',{'profile_form':profile_form})
+
+    else:
+        messages.success(request,('You gats be logged in !!'))
+        return redirect('home')
+    
+
+
+
     
